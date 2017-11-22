3b6947c31ed58440fad79721c85f0e6d7946a682
bybai <bybai@cn.ibm.com>
2017-11-22 04:02:26 -0500

    support mysql,postgresql,sqllite

--- xCAT-python-client/xcclient/inventory/dbsession.py
+++ xCAT-python-client/xcclient/inventory/dbsession.py
@@ -1,8 +1,30 @@
 from sqlalchemy import create_engine,inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-engine=create_engine('postgresql+psycopg2://xcatadm:cluster@localhost/xcatdb',echo=False)
+if os.path.exists('/etc/xcat/cfgloc'):
+    db_info = open('/etc/xcat/cfgloc')
+else:
+    engine_value = 'sqlite:///:/etc/xcat'
+try:
+     all_the_text = db_info.read( )
+     db_line = all_the_text.strip()
+     db_type = db_line.split(':')[0]
+     if db_type == 'Pg':
+         conn="postgresql+psycopg2://"
+     elif db_type == 'mysql':
+         conn="mysql+pymysql://"
+         
+     dbname = db_line.split(';')[0].split('=')[-1]
+     db_str = db_line.split(';')[-1].split('=')[-1].split('|')
+     dbinfo = db_str[1]+":"+db_str[2]+"@"+db_str[0]+"/"+dbname
+     engine_value = conn + dbinfo
+     
+finally:
+     db_info.close( )
+
+engine=create_engine(engine_value,echo=False)
 Base = declarative_base(engine)
 
 def loadSession():
@@ -11,3 +33,4 @@ def loadSession():
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
+
