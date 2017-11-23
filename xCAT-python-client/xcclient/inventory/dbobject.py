e943f76145a1710ff2813773461be415ccf5d01a
immarvin <yangsbj@cn.ibm.com>
2017-11-23 04:16:42 -0500

    refine the code arch of dbobject.py

--- xCAT-python-client/xcclient/inventory/dbobject.py
+++ xCAT-python-client/xcclient/inventory/dbobject.py
@@ -2,124 +2,73 @@ from dbsession import *
 from copy import *
 import pdb
 
+class mixin(object):
+    def getdict(self):
+        mydict={}
+        for mykey in self.__dict__.keys():
+           if mykey in self.__table__.columns:
+              mydict[self.__tablename__+'.'+mykey.encode()]= self.__dict__[mykey] if self.__dict__[mykey] is None else self.__dict__[mykey].encode()
+        try:
+            self.__class__.outprocess(mydict)
+        except:
+            pass 
+        return mydict
 
 ########################################################################
-class Password(Base):
+class Password(Base,mixin):
     """"""
     __tablename__ = 'passwd'
     __table_args__ = {'autoload':True}
 
 ########################################################################
-class Networks(Base):
+class Networks(Base,mixin):
     """"""
     __tablename__ = 'networks'
     __table_args__ = {'autoload':True}
-    def __getattr__(self, key):
-        if key == '_instance_dict' and key not in self.__dict__:
-            self._sa_instance_state.initialize(key)
-            return getattr(self, key)
-        try:
-            return self._instance_dict[key]
-        except KeyError:
-            return Base.__getattr__(self, key)
-
 
 ########################################################################
-class Nodetype(Base):
+class Nodetype(Base,mixin):
     """"""
     __tablename__ = 'nodetype'
     __table_args__ = {'autoload':True} 
-    def __getattr__(self, key):
-        if key == '_instance_dict' and key not in self.__dict__:
-            self._sa_instance_state.initialize(key)
-            return getattr(self, key)
-        try:
-            return self._instance_dict[key]
-        except KeyError:
-            return Base.__getattr__(self, key)
 
 ########################################################################
-class Hosts(Base):
+class Hosts(Base,mixin):
     """"""
     __tablename__ = 'hosts'
     __table_args__ = {'autoload':True}
-    def __getattr__(self, key):
-        if key == '_instance_dict' and key not in self.__dict__:
-            self._sa_instance_state.initialize(key)
-            return getattr(self, key)
-        try:
-            return self._instance_dict[key]
-        except KeyError:
-            return Base.__getattr__(self, key)
 
 ########################################################################
-class Noderes(Base):
+class Noderes(Base,mixin):
     """"""
     __tablename__ = 'noderes'
     __table_args__ = {'autoload':True}
-    def __getattr__(self, key):
-        if key == '_instance_dict' and key not in self.__dict__:
-            self._sa_instance_state.initialize(key)
-            return getattr(self, key)
-        try:
-            return self._instance_dict[key]
-        except KeyError:
-            return Base.__getattr__(self, key)
 
 ########################################################################
-class Switch(Base):
+class Switch(Base,mixin):
     """"""
     __tablename__ = 'switch'
     __table_args__ = {'autoload':True}
-    def __getattr__(self, key):
-        if key == '_instance_dict' and key not in self.__dict__:
-            self._sa_instance_state.initialize(key)
-            return getattr(self, key)
-        try:
-            return self._instance_dict[key]
-        except KeyError:
-            return Base.__getattr__(self, key)
 ########################################################################
-class Mac(Base):
+class Mac(Base,mixin):
     """"""
     __tablename__ = 'mac'
     __table_args__ = {'autoload':True}
-    def __getattr__(self, key):
-        if key == '_instance_dict' and key not in self.__dict__:
-            self._sa_instance_state.initialize(key)
-            return getattr(self, key)
-        try:
-            return self._instance_dict[key]
-        except KeyError:
-            return Base.__getattr__(self, key)
+    @classmethod
+    def outprocess(self,mydict):
+        key=self.__tablename__+'.'+'mac'
+        mydict[key]=mydict[key].split('|')
 ########################################################################
-class Postscripts(Base):
+class Postscripts(Base,mixin):
     """"""
     __tablename__ = 'postscripts'
     __table_args__ = {'autoload':True}
     
-    def __getattr__(self, key):
-        if key == '_instance_dict' and key not in self.__dict__:
-            self._sa_instance_state.initialize(key)
-            return getattr(self, key)
-        try:
-            return self._instance_dict[key]
-        except KeyError:
-            return Base.__getattr__(self, key)
 ########################################################################
-class Bootparams(Base):
+class Bootparams(Base,mixin):
     """"""
     __tablename__ = 'bootparams'
     __table_args__ = {'autoload':True}
-
-    def __getattr__(self, key):
-        if key == '_instance_dict' and key not in self.__dict__:
-            self._sa_instance_state.initialize(key)
-        return getattr(self, key)
-        try:
-            return self._instance_dict[key]
-        except KeyError:
-            return Base.__getattr__(self, key)
 
 
 #----------------------------------------------------------------------
@@ -127,25 +76,18 @@ class Bootparams(Base):
 def query_table_by_node(session, tclass, tkey):
     """"""
     result=session.query(tclass).filter(tclass.node == tkey).all()
-    return result
+    return result[0].getdict()
 
 
 def query_nodelist_by_key(session, nodelist):
     """"""
     nodelist_value = {}
     for node in nodelist:
-        nodeattr = {}
+        nodelist_value[node]={}
         classlist = [Bootparams,Nodetype,Hosts,Switch,Mac,Noderes]
         for eachclass in classlist:
-            table_row = query_table_by_node(session,eachclass,node)
-            tlist = eachclass.__table__.columns
-            for t in tlist:
-                v=t.name
-                for item in table_row:
-                    if item.__dict__[v]:
-                         nodeattr[str(t)] = item.__dict__[v].encode()
-
-        nodelist_value[node]=nodeattr
+            clsdict = query_table_by_node(session,eachclass,node)
+            nodelist_value[node].update(clsdict)
     return nodelist_value
 
 if __name__ == "__main__":
