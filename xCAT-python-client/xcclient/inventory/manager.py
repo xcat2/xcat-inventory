df47d751ed582f7686e1d554edb7153247f6180d
Bin Xu <bxuxa@cn.ibm.com>
2017-11-24 17:44:58 +0800

    Refactory as new integrated design

--- xCAT-python-client/xcclient/inventory/manager.py
+++ xCAT-python-client/xcclient/inventory/manager.py
@@ -5,11 +5,14 @@
 # -*- coding: utf-8 -*-
 #
 
-import dbsession
+from __future__ import print_function
+
+from dbfactory import dbfactory
 from xcatobj import *
 from xcclient.shell import CommandException
 
 import os
+import yaml
 
 """
 Command-line interface to xCAT inventory import/export
@@ -17,6 +20,71 @@ Command-line interface to xCAT inventory import/export
 
 VALID_OBJ_TYPES = ['site', 'node', 'network', 'osimage']
 VALID_OBJ_FORMAT = ['yaml', 'json']
+
+class InventoryFactory(object):
+    __InventoryHandlers__ = {}
+    __InventoryClass__ = {'node': Node}
+    __db__ = None
+
+    def __init__(self, objtype):
+        self.objtype = objtype
+
+    @staticmethod
+    def createHandler(objtype):
+        # non-thread-safe now
+        if objtype not in InventoryFactory.__InventoryHandlers__:
+            InventoryFactory.__InventoryHandlers__[objtype] = InventoryFactory(objtype)
+
+        return InventoryFactory.__InventoryHandlers__[objtype]
+
+    def getDBInst(self):
+        if InventoryFactory.__db__ is None:
+            InventoryFactory.__db__=dbfactory()
+
+        return InventoryFactory.__db__
+
+    def exportObjs(self, objlist, fmt, location):
+        myclass = InventoryFactory.__InventoryClass__[self.objtype]
+        #myclass.loadschema()
+        myclass.loadschema(os.path.join(os.path.dirname(__file__), 'node.yaml'))
+        #tabs = myclass.getTables()
+        tabs = ['nodetype', 'switch', 'hosts', 'mac', 'noderes', 'postscripts', 'bootparams']
+        obj_attr_dict = self.getDBInst().gettab(tabs, objlist)
+        for key, attrs in obj_attr_dict.items():
+            newobj = myclass.createfromdb(key, attrs)
+            if not fmt or fmt.lower() == 'json':
+                self.dump2json(newobj.getobjdict())
+            else:
+                self.dump2yaml(newobj.getobjdict())
+
+    def exportObj(self):
+        pass
+
+    def importObjs(self,objlist,location):
+        with open(location) as json_data:
+            d = json.load(json_data)
+            print(d)
+        return
+        myclass = __InventoryClass__[self.objtype]
+        #myclass.loadschema()
+        myclass.loadschema(os.path.join(os.path.dirname(__file__), 'node.yaml'))
+
+        self.getDBInst.gettab(tabs, objlist)
+        for key, attrs in obj_attr_dict.items():
+            newobj = myclass.createfromfile(key, attrs)
+            if fmt.lower() == 'json':
+                self.dump2json(newobj.setDict())
+            else:
+                self.dump2yaml(newobj.setDict())
+
+    def importObj(self):
+        pass
+
+    def dump2yaml(self, xcatobj):
+        print(yaml.dump(xcatobj, default_flow_style=False))
+
+    def dump2json(self, xcatobj):
+        print(json.dumps(xcatobj, sort_keys=True, indent=4, separators=(',', ': ')))
 
 def validate_args(args, action):
 
@@ -34,27 +102,28 @@ def validate_args(args, action):
         if args.format and args.format.lower() not in VALID_OBJ_FORMAT:
             raise CommandException("Invalid exporting format: %(f)s", f=args.format)
 
-def export_by_type(objtype, name, location, fmt):
-    if name:
-        print("specified dedicate object: %s" % name)
+def export_by_type(objtype, names, location, fmt):
+    hdl = InventoryFactory.createHandler(objtype)
+    objlist = []
+    if names:
+        objlist.extend(names.split(','))
+
+    hdl.exportObjs(objlist, fmt, location)
 
 def export_all(location, fmt):
-    session = dbsession.loadSession()
-    nodelist = ['c910f03c17k41','c910f03c17k42']
-    nodelist_value = query_nodelist_by_key(session, nodelist)
+    for objtype in ['node']:#VALID_OBJ_TYPES:
+        hdl = InventoryFactory.createHandler(objtype)
+        hdl.exportObjs([], fmt, location)
 
-    Node.loaddb(nodelist_value)
-    Node.loadschema(os.path.join(os.path.dirname(__file__), 'node.yaml'))
-    for node in Node.listobj():
-        if fmt in ['yaml', 'YAML']:
-            Node(node).dump2yaml()
-        else:
-            Node(node).dump2json()
+def import_by_type(objtype, names, location):
+    hdl = InventoryFactory.createHandler(objtype)
+    objlist = []
+    if names:
+        objlist.extend(names.split(','))
 
-def import_by_type(objtype, name, location):
-    print("import type of %s inventory from %s" % (objtype, location))
-    if name:
-        print("specified dedicate object: %s" % name)
+    hdl.importObjs(objlist, location)
 
 def import_all(location):
-    print("import inventory from %s" % location)
+    for objtype in ['node']:#VALID_OBJ_TYPES:
+        hdl = InventoryFactory.createHandler(objtype)
+        hdl.importObjs([], location)
