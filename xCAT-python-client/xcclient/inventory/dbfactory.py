a57f4b5a315180fffc02486a3ec257acf8d038c1
ertaozh <ertaozh@cn.ibm.com>
2017-11-29 21:37:41 -0500

    Enble the function to insert new entry in a tab

--- xCAT-python-client/xcclient/inventory/dbfactory.py
+++ xCAT-python-client/xcclient/inventory/dbfactory.py
@@ -36,12 +36,14 @@ class dbfactory():
         def create_or_update(session,tabcls,key,newdict):
             tabkey=tabcls.getkey()
             record=session.query(tabcls).filter(getattr(tabcls,tabkey).in_([key])).all()
-            if record is not None:
+            if record:
                 session.query(tabcls).filter(getattr(tabcls,tabkey) == key).update(newdict)
             else:
                 print "not found"
                 print dbdict
-                                
+                newdict[tabkey]=key
+                session.execute(tabcls.__table__.insert(), newdict)
+                session.commit()
                 
         if self.dbsession is None or dbdict is None:
             return None
@@ -69,6 +71,9 @@ class dbfactory():
 if __name__ == "__main__":
     df1=dbfactory()
     mydict=df1.gettab(['mac'],["node0001","node0002"])
+    if not mydict:
+        mydict={}
+        mydict["node0001"]={}
     mydict["node0001"]['mac.comments']="zzzzz"
     mydict["node0001"]['mac.interface']="mmmmmmmmmm"
 
