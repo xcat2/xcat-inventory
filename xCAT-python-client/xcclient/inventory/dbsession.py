72b3110a6bdb5fa2b13728fa4d2cc342d9559b4b
immarvin <yangsbj@cn.ibm.com>
2017-11-30 03:48:19 -0500

    refine dbsession, support mysql

--- xCAT-python-client/xcclient/inventory/dbsession.py
+++ xCAT-python-client/xcclient/inventory/dbsession.py
@@ -8,28 +8,29 @@
 from sqlalchemy import create_engine,inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import re
 import os
 
+
+dbcfgpath='/etc/xcat/cfgloc'
+dbcfgregex=re.compile("^(\S+):dbname=(\S+);host=(\S+)\|(\S+)\|(\S*)$")
+
 engine_value = 'sqlite:///:/etc/xcat'
-if os.path.exists('/etc/xcat/cfgloc'):
+if os.path.exists(dbcfgpath):
+    dbcfgfile = open(dbcfgpath)
+    dbcfgloc = dbcfgfile.read( )
+    dbcfgfile.close()
     try:
-        db_info = open('/etc/xcat/cfgloc')
-        all_the_text = db_info.read( )
-        db_line = all_the_text.strip()
-        db_type = db_line.split(':')[0]
-        if db_type == 'Pg':
-            conn="postgresql+psycopg2://"
-        elif db_type == 'mysql':
-            conn="mysql+pymysql://"
- 
-        dbname = db_line.split(';')[0].split('=')[-1]
-        db_str = db_line.split(';')[-1].split('=')[-1].split('|')
-        dbinfo = db_str[1]+":"+db_str[2]+"@"+db_str[0]+"/"+dbname
-        engine_value = conn + dbinfo
+       (dbtype,dbname,dbhost,dbusername,dbpasswd)=re.findall(dbcfgregex,dbcfgloc)[0]
+    except:
+       print "Fatal: invalid cfgloc file "+dbcfgpath+"!"
+       exit (1)
+    if dbtype == 'Pg':
+        conn="postgresql+psycopg2://"
+    elif dbtype == 'mysql':
+        conn="mysql+pymysql://"
+    engine_value = conn+dbusername+':'+dbpasswd+'@'+dbhost+'/'+dbname
          
-    finally:
-         db_info.close( )
 
 engine=create_engine(engine_value,echo=False)
 Base = declarative_base(engine)
