0c121cfeaa34cb15ba01e16343f642a4d3dce383
bybai <bybai@cn.ibm.com>
2017-12-12 02:56:42 -0500

    support mysql,sqlite

--- xCAT-python-client/xcclient/inventory/dbsession.py
+++ xCAT-python-client/xcclient/inventory/dbsession.py
@@ -14,29 +14,46 @@ import os
 
 dbcfgpath='/etc/xcat/cfgloc'
 dbcfgregex=re.compile("^(\S+):dbname=(\S+);host=(\S+)\|(\S+)\|(\S*)$")
+Base = declarative_base()
+Base.metadata.bind = None;
 
-engine_value = 'sqlite:///:/etc/xcat'
-if os.path.exists(dbcfgpath):
-    dbcfgfile = open(dbcfgpath)
-    dbcfgloc = dbcfgfile.read( )
-    dbcfgfile.close()
-    try:
-       (dbtype,dbname,dbhost,dbusername,dbpasswd)=re.findall(dbcfgregex,dbcfgloc)[0]
-    except:
-       print "Fatal: invalid cfgloc file "+dbcfgpath+"!"
-       exit (1)
-    if dbtype == 'Pg':
-        conn="postgresql+psycopg2://"
-    elif dbtype == 'mysql':
-        conn="mysql+pymysql://"
-    engine_value = conn+dbusername+':'+dbpasswd+'@'+dbhost+'/'+dbname
-         
+def createEngine(tablename=None):
+    if os.path.exists(dbcfgpath):
+        dbcfgfile = open(dbcfgpath)
+        dbcfgloc = dbcfgfile.read( )
+        dbcfgfile.close()
+        try:
+            (dbtype,dbname,dbhost,dbusername,dbpasswd)=re.findall(dbcfgregex,dbcfgloc)[0]
+        except:
+            print "Fatal: invalid cfgloc file "+dbcfgpath+"!"
+            exit (1)
+        if dbtype == 'Pg':
+            conn="postgresql+psycopg2://"
+        elif dbtype == 'mysql':
+            conn="mysql+pymysql://"
+        engine_value = conn+dbusername+':'+dbpasswd+'@'+dbhost+'/'+dbname
+    elif tablename:
+        engine_value = 'sqlite:////etc/xcat/'+tablename+'.sqlite'
+    else:
+        print "Fatal: empty table name"
+        exit (1)
+    engine=create_engine(engine_value,echo=False)
+    return engine
 
-engine=create_engine(engine_value,echo=False)
-Base = declarative_base(engine)
-
-def loadSession():
+def getEngine(tablename=None):
     """"""
+    if os.path.exists(dbcfgpath):
+        if not Base.metadata.bind:
+            engine=createEngine()
+        else:
+            return Base.metadata.bind 
+    elif tablename:
+        engine=createEngine(tablename)
+    return engine
+
+def loadSession(tablename=None):
+    """"""
+    engine = getEngine(tablename)
     metadata = Base.metadata
     Session = sessionmaker(bind=engine)
     session = Session()
