3b1d76ce1a43561dae352587d7b9461dd3d6d89a
SONG YANG <yangsbj@cn.ibm.com>
2018-01-17 11:17:45 +0800

    add exception definitions (#4)

---
+++ xcat-inventory/xcclient/inventory/exceptions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+###############################################################################
+# IBM(c) 2007 EPL license http://www.eclipse.org/legal/epl-v10.html
+###############################################################################
+# -*- coding: utf-8 -*-
+#
+
+"""
+Exception definitions for xCAT inventory import/export
+"""
+import sys
+import traceback
+
+class BaseException(Exception):
+    """Base Command Exception."""
+
+    message = "An unknown error occurred."
+
+    def __init__(self, message=None, **kwargs):
+        if message:
+            self.message = message
+        try:
+            self._error_msg = self.message % kwargs
+        except Exception:
+            self._error_msg = self.message
+
+    def __str__(self):
+        return self._error_msg
+
+class ObjNonExistException(BaseException):
+    pass
+class CommandException(BaseException):
+    pass
+class InvalidFileException(BaseException):
+    pass
