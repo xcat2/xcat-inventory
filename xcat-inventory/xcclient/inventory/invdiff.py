43b49a6dd4a9116d171f072fb00c97fb38f40044
yangsbj <yangsbj@cn.ibm.com>
2018-08-01 06:39:52 -0400

    refine invdiff

--- xcat-inventory/xcclient/inventory/invdiff.py
+++ xcat-inventory/xcclient/inventory/invdiff.py
@@ -64,13 +64,27 @@ def dump2json(xcatobj, filename=None):
 def json2dict(jsonstr):
     return json.loads(jsonstr)
 
-if(len(sys.argv)!=3):
+if(len(sys.argv)!=4):
     print("invdiff <file1> <file2>",file=sys.stderr)
     exit(1)
 
 print("\n====================BEGIN=====================\n")
 fn1=sys.argv[1]
 fn2=sys.argv[2]
+fn3=sys.argv[3]
+
+#print("file1=%s"%(fn1))
+#print("file2=%s"%(fn2))
+
+if fn1=="/dev/null":
+    print("--- %s"%(fn1),file=sys.stdout)
+    print("+++ %s\n"%(fn3),file=sys.stdout)
+    exit(0)
+
+if fn2=="/dev/null":
+    print("--- %s"%(fn3),file=sys.stdout)
+    print("+++ %s\n"%(fn2),file=sys.stdout)
+    exit(0)
 
 
 try:
@@ -79,19 +93,20 @@ try:
 except InvalidFileException,e:
     (retcode,out,err)=runCommand("diff -u %s %s"%(fn1,fn2))    
     if out:
-        out=re.sub(r"%s"%(fn1),fn2,out)
+        out=re.sub(r"%s"%(fn2),fn3,out)
         print(out,file=sys.stdout) 
     if err:
-        err=re.sub(r"%s"%(fn1),fn2,err)
+        err=re.sub(r"%s"%(fn2),fn3,err)
         print(err,file=sys.stderr) 
     print("\n===================END======================\n")
     exit(retcode)
 
 
 dt=deepdiff.DeepDiff(d1,d2,ignore_order=True,report_repetition=False,exclude_paths='',significant_digits=None,view='text',verbose_level=2)
-print("\n--- %s\n+++ %s"%(fn2,fn2))
 
-
-
-print(dump2yaml(dict(dt)))
+print("\n--- %s\n+++ %s"%(fn3,fn3))
+pprint.pprint(dt,indent=2)
+#exit(0)
+#print(dump2yaml(json.loads(dt.json)))
+#print(dump2yaml((dt.json)))
 print("\n====================END=====================\n")
