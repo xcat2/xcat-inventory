03a38c29d00704da76a426e624edad495452d102
binxu <bxuxa@cn.ibm.com>
2019-04-29 13:10:20 +0800

    using types to filter the inventory class

--- xcat-inventory/xcclient/allien/resources/inventory.py
+++ xcat-inventory/xcclient/allien/resources/inventory.py
@@ -4,8 +4,9 @@
 
 # -*- coding: utf-8 -*-
 
-from flask_restplus import Resource, Namespace, fields
-from xcclient.inventory.manager import export_by_type
+from flask import request, current_app
+from flask_restplus import Resource, Namespace, fields, reqparse
+from xcclient.inventory.manager import export_by_type, importobj
 
 ns = Namespace('inventory', ordered=True, description='Inventory Management')
 
@@ -18,23 +19,65 @@ inv_resource = ns.model('Inventory', {
 })
 
 
+def _split_inventory_types(types):
+
+    include = list()
+    exclude = ['credential']
+
+    # get the include and exclude
+    for rt in types:
+        rt = rt.strip()
+        if rt.startswith('-'):
+            exclude.append(rt[1:])
+        else:
+            include.append(rt)
+
+    return include, exclude
+
+
 @ns.route('/')
 class InventoryResource(Resource):
 
+    @ns.doc('export_inventory')
+    @ns.param('types', 'exported inventory types')
     def get(self):
         """get all inventory defined in store"""
-        # TODO, support query parameter
-        return export_by_type(None, None, destfile=None, destdir=None, fmt='dict', version=None, exclude=['credential'])
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('types', action='split', help="Exported inventory types, started with '-' means to exclude")
+        args = parser.parse_args()
+
+        include = None
+        exclude = None
+        if args.get('types'):
+            include, exclude = _split_inventory_types(args.get('types'))
+            if include:
+                include = ','.join(include)
+
+        return export_by_type(include, None, destfile=None, destdir=None, fmt='dict', version=None, exclude=exclude)
 
     @ns.doc('import_inventory')
+    @ns.param('types', 'imported inventory types')
     @ns.expect(inv_resource)
     @ns.response(201, 'Inventory successfully imported.')
     def post(self):
         """import inventory objects"""
         data = request.get_json()
 
-        # TODO: call inventory to store in DB
+        parser = reqparse.RequestParser()
+        parser.add_argument('types', action='split', help="Imported inventory types, started with '-' means to exclude")
+        parser.add_argument('clean', action='split', help="Imported inventory types, started with '-' means to exclude")
+        args = parser.parse_args()
+
+        include = None
+        exclude = None
+        if args.get('types'):
+            include, exclude = _split_inventory_types(args.get('types'))
+            if include:
+                include = ','.join(include)
+
+        importobj(None, None, include, None, dryrun=False, version=None,
+                  update=args.get('clean'), envs=args.get('environs'), env_files=None, exclude=args.exclude.split(','))
 
         return None, 201
-
 
