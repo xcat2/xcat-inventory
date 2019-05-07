9ad6312171b6d2352561ae010d4d44da37778e22
cxhong <cxhong@us.ibm.com>
2019-05-07 09:53:36 -0400

    Add network/route API

--- xcat-inventory/xcclient/allien/resources/network.py
+++ xcat-inventory/xcclient/allien/resources/network.py
@@ -6,7 +6,9 @@
 
 from flask import request, current_app
 from flask_restplus import Resource, Namespace, fields, reqparse
-from ..invmanager import get_inventory_by_type, upd_inventory_by_type, del_inventory_by_type, transform_from_inv, transform_to_inv
+from xcclient.xcatd import XCATClient, XCATClientParams
+from xcclient.xcatd.client.xcat_exceptions import XCATClientError
+from ..invmanager import get_inventory_by_type, upd_inventory_by_type, del_inventory_by_type, transform_from_inv, transform_to_inv, validate_resource_input_data, patch_inventory_by_type
 from ..invmanager import InvalidValueException, ParseException
 from .inventory import ns, resource
 
@@ -14,12 +16,16 @@ from .inventory import ns, resource
 These APIs is to handle networking related resources: subnet, route.
 """
 
+patch_action = ns.model('modify', {
+    'modify': fields.Raw(description='The update attr and value info of resource', required=True)
+})
+
 
 @ns.route('/subnets')
 class NetworkListResource(Resource):
 
     def get(self):
-        """get networks defined in store"""
+        """get all the networks defined in store"""
         return transform_from_inv(get_inventory_by_type('network'))
 
     @ns.doc('create_subnet')
@@ -29,6 +35,7 @@ class NetworkListResource(Resource):
         """create a network object"""
         data = request.get_json()
 
+        # TODO: better to handle the exceptions
         try:
             upd_inventory_by_type('network', transform_to_inv(data))
         except (InvalidValueException, ParseException) as e:
@@ -38,11 +45,11 @@ class NetworkListResource(Resource):
 
 
 @ns.route('/subnets/<string:name>')
-@ns.response(404, 'Subnet not found.')
+@ns.response(404, 'network not found.')
 class NetworkResource(Resource):
 
     def get(self, name):
-        """get specified network resource"""
+        """get a specified network resource"""
         result = get_inventory_by_type('network', [name])
         if not result:
             ns.abort(404)
@@ -50,32 +57,50 @@ class NetworkResource(Resource):
         return transform_from_inv(result)[-1]
 
     def delete(self, name):
-        """delete a subnet object"""
-        del_inventory_by_type('network', [name])
+        """delete a specified network resource"""
+        try:
+          del_inventory_by_type('network', [name])
+        except (XCATClientError) as e:
+            ns.abort(400, str(e))
 
+        return None, 200
+
+    @ns.expect(resource)
     def put(self, name):
-        """modify a subnet object"""
+        """replace a specified network resource"""
         data = request.get_json()
         # TODO, need to check if the name is consistent
         try:
+            validate_resource_input_data(data, name)
             upd_inventory_by_type('network', transform_to_inv(data))
         except (InvalidValueException, ParseException) as e:
             ns.abort(400, e.message)
 
         return None, 200
 
+    @ns.expect(patch_action)
+    def patch(self, name):
+        """Modify a specified network resources"""
+        data = request.get_json()
+        try:
+            patch_inventory_by_type('network', name, data)
+        except (InvalidValueException, XCATClientError) as e:
+            ns.abort(400, str(e))
+
+        return None, 201
+
 
 @ns.route('/routes')
 class RoutesListResource(Resource):
 
     def get(self):
-        """get routes defined in store"""
+        """get all the routes defined in store"""
         return transform_from_inv(get_inventory_by_type('route'))
 
     @ns.expect(resource)
     @ns.response(201, 'Route successfully created.')
     def post(self):
-        """create a static route object"""
+        """create a static route """
         data = request.get_json()
 
         try:
@@ -91,7 +116,7 @@ class RoutesListResource(Resource):
 class RouteResource(Resource):
 
     def get(self, name):
-        """get specified route with name"""
+        """get a specified route in store"""
         result = get_inventory_by_type('route', [name])
         if not result:
             ns.abort(404, "Route {} doesn't exist".format(name))
@@ -99,16 +124,34 @@ class RouteResource(Resource):
         return transform_from_inv(result)[-1]
 
     def delete(self, name):
-        """delete a route object"""
-        del_inventory_by_type('network', [name])
-
-    def put(self, name):
-        """modify a route object"""
-        data = request.get_json()
-        # TODO, need to check if the name is consistent
+        """delete a specified route """
         try:
+            del_inventory_by_type('route', [name])
+        except (XCATClientError) as e:
+            ns.abort(400, str(e))
+
+        return None, 200
+
+
+    @ns.expect(resource)
+    def put(self, name):
+        """replace a specified route """
+        data = request.get_json()
+        try:
+            validate_resource_input_data(data, name)
             upd_inventory_by_type('route', transform_to_inv(data))
         except (InvalidValueException, ParseException) as e:
             ns.abort(400, e.message)
 
         return None, 200
+
+    @ns.expect(patch_action)
+    def patch(self, name):
+        """Modify a specified route """
+        data = request.get_json()
+        try:
+            patch_inventory_by_type('route', name, data)
+        except (InvalidValueException, XCATClientError) as e:
+            ns.abort(400, str(e))
+
+        return None, 201
